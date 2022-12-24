41effded77f092628153f2b8ce78a08e86ac7a94
BogdanTeryukhov <94743986+BogdanTeryukhov@users.noreply.github.com>
2022-12-24 16:21:50 +0300

    add Lab9

---
+++ LabaSeti9/entity/video.go
@@ -0,0 +1,7 @@
+package entity
+
+type Video struct {
+	Title       string `json:"title" binding:"min=2,max=100"`
+	Description string `json:"description" binding:"max=200"`
+	URL         string `json:"url" binding:"required,url"`
+}
---
+++ LabaSeti9/sever.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"LabaSeti9/controller"
+	"LabaSeti9/middlewares"
+	"LabaSeti9/service"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
+
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+			}
+
+		})
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
+
+	server.Run(":8080")
+}
